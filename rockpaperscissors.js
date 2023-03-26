740407726872d59e30c36f6505991aeada5f8197
blackpantherx8x <109686439+blackpantherx8x@users.noreply.github.com>
2023-03-26 14:56:56 +0200

    Function to generate a random choice for the computer - rockpaperscissors.js

---
+++ rockpaperscissors.js
@@ -0,0 +1,7 @@
+// Function to generate a random choice for the computer
+function computerPlay() {
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+  }
+  
