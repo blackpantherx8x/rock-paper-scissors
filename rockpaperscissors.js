89afa8f9d1e436e71f48f6d4827775114573d891
blackpantherx8x <109686439+blackpantherx8x@users.noreply.github.com>
2023-03-26 15:17:46 +0200

    Function to play a single round of the game - rockpaperscissors.js

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -5,3 +5,17 @@ function computerPlay() {
     return choices[randomIndex];
   }
   
+// Function to play a single round of the game
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+      return 'It\'s a tie!';
+    } else if (
+      (playerSelection === 'rock' && computerSelection === 'scissors') ||
+      (playerSelection === 'paper' && computerSelection === 'rock') ||
+      (playerSelection === 'scissors' && computerSelection === 'paper')
+    ) {
+      return 'You win!';
+    } else {
+      return 'Computer wins!';
+    }
+  }
